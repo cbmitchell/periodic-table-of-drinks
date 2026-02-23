a6d0dac612f25b3df8e2696de3d635a70ed4270a
Chris Mitchell <bobtheawesomechicken@gmail.com>
2026-02-23 10:43:32 -0600

    A little more cleaning up of the DrinkCell component

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,34 @@ import { ThemeProvider } from "@mui/material/styles";
 import { lightTheme } from "./theme";
 
 const ingredients = [
-	'1 ½ oz chocolate vodka',
-	'1 ½ oz Irish cream',
-	'1 ½ oz Kahlua',
-	'1 oz creme de cacao',
-	'48 oz chocolate ice cream',
-	'Chocolate syrup',
-	'Chocolate sprinkles',
-]
+	"1 ½ oz chocolate vodka",
+	"1 ½ oz Irish cream",
+	"1 ½ oz Kahlua",
+	"1 oz creme de cacao",
+	"48 oz chocolate ice cream",
+	"Chocolate syrup",
+	"Chocolate sprinkles",
+];
 
 const instructions = [
-	'Combine ingredients in blender',
-	'Cover and blend well',
-	'Put chocolate sprinkles on a dish',
-	'Coat rim of glass in chocolate syrup',
-	'Touch rim of glass to sprinkles',
-]
+	"Combine ingredients in blender",
+	"Cover and blend well",
+	"Put chocolate sprinkles on a dish",
+	"Coat rim of glass in chocolate syrup",
+	"Touch rim of glass to sprinkles",
+];
 
 export default function App() {
 	return (
 		<ThemeProvider theme={lightTheme}>
 			<DrinkCell
-				title = 'Frozen Chocolate Mudslide'
-				abbreviation = 'Cm'
-				icon = 'hurricane'
-				ingredients = {ingredients}
-				instructions = {instructions}
+				title="Frozen Chocolate Mudslide"
+				abbreviation="Cm"
+				icon="hurricane"
+				ingredients={ingredients}
+				instructions={instructions}
+        group="actinides"
 			/>
 		</ThemeProvider>
-	)
+	);
 }
--- src/components/DrinkCell.tsx
+++ src/components/DrinkCell.tsx
@@ -1,4 +1,13 @@
-import { Box, Card, CardContent, Grid, List, ListItem, ListItemText, Typography } from "@mui/material";
+import {
+	Box,
+	Card,
+	CardContent,
+	Grid,
+	List,
+	ListItem,
+	ListItemText,
+	Typography,
+} from "@mui/material";
 
 export type GlassIconName =
 	| "collins"
@@ -14,22 +23,47 @@ export type GlassIconName =
 	| "tiki"
 	| "wine";
 
+const glassIconBasePath = "glass-icons/black/";
+
 const glassIcons: Record<GlassIconName, string> = {
-	collins: "",
-	coup: "",
-	flute: "",
-	hurricane: "glass-icons/black/hurricane.png",
-	irish: "",
-	margarita: "",
-	martini: "",
-	mule: "",
-	pint: "",
-	rocks: "",
-	tiki: "",
-	wine: "",
+	collins: "collins.png",
+	coup: "coup.png",
+	flute: "flute.png",
+	hurricane: "hurricane.png",
+	irish: "irish.png",
+	margarita: "margarita.png",
+	martini: "martini.png",
+	mule: "mule.png",
+	pint: "pint.png",
+	rocks: "rocks.png",
+	tiki: "tiki.png",
+	wine: "wine.png",
 };
 
-const debugColors = true
+export type ElementGroup =
+	| "alkali_metals"
+	| "alkaline_earth_metals"
+	| "transition_metals"
+	| "post_transition_metals"
+	| "metalloids"
+	| "nonmetals"
+	| "halogens"
+	| "noble_gases"
+	| "lanthanides"
+	| "actinides";
+
+const elementGroupColors: Record<ElementGroup, string> = {
+	alkali_metals: "red",
+	alkaline_earth_metals: "orange",
+	transition_metals: "yellow",
+	post_transition_metals: "teal",
+	metalloids: "green",
+	nonmetals: "purple",
+	halogens: "cyan",
+	noble_gases: "pink",
+	lanthanides: "lightgreen",
+	actinides: "lavender",
+};
 
 interface DrinkCellProps {
 	title: string;
@@ -37,7 +71,11 @@ interface DrinkCellProps {
 	icon: GlassIconName;
 	ingredients: string[];
 	instructions?: string[];
+	group?: ElementGroup;
 }
+
+const CELL_WIDTH = 256;
+const CELL_HEIGHT = 320;
 
 export function DrinkCell({
 	title,
@@ -45,110 +83,79 @@ export function DrinkCell({
 	icon,
 	ingredients,
 	instructions = [""],
+	group = "actinides",
 }: DrinkCellProps) {
 	return (
-		<Card sx={{ width: 256, height: 320, backgroundColor: 'lightblue' }}>
+		<Card
+			sx={{
+				width: CELL_WIDTH,
+				height: CELL_HEIGHT,
+				backgroundColor: elementGroupColors[group],
+			}}
+		>
 			<CardContent>
-        <Grid container spacing={0}>
-          <Grid size={8}>
-            <Box>
-              <Typography sx={{ 
-                // backgroundColor: "lightGrey",
-                fontWeight: "medium",
-                minHeight: 48,
-                lineHeight: 1.2,
-              }}>
-                {title}
-              </Typography>
-              <Typography sx={{
-                fontSize: 80,
-                fontWeight: 'bold',
-                lineHeight: 0.8,
-                // backgroundColor: "lightGreen",
-                height: 64,
-
-              }}>
-                {abbreviation}
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid size={4} sx={{ position: 'relative' }}>
-            <Box
-              component='img'
-              src={glassIcons[icon]}
-              sx={{
-                // backgroundColor: 'pink',
-                width: '100%',
-                position: 'absolute',
-                bottom: 0,
-              }}>
-            </Box>
-          </Grid>
-          <Grid size={6}>
-            <Box sx={{p: 1}}>
-              <List
-                dense 
-                sx={{ 
-                  listStyleType: 'disc', 
-                  pl: 0,
-                }}
-              >
-                {ingredients.map((ingredient) => (
-                  <ListItem key={ingredient} disablePadding>
-                    <ListItemText
-                      sx={{
-                        fontSize: 8,
-                        display: 'list-item',
-                        my: 0,
-                      }}
-                      primary={ingredient}
-                      slotProps={{
-                        primary: {
-                          sx: {
-                            // backgroundColor: 'lightgrey',
-                            fontSize: '8pt',
-                          }
-                        }
-                      }}
-                    />
-                  </ListItem>
-                ))}
-              </List>
-            </Box>
-          </Grid>
-          <Grid size={6}>
-            <Box sx={{p: 1}}>
-              <List
-                dense 
-                sx={{ 
-                  listStyleType: 'disc', 
-                  pl: 0,
-                }}
-              >
-                {instructions.map((instruction) => (
-                  <ListItem key={instruction} disablePadding>
-                    <ListItemText
-                      sx={{
-                        fontSize: 8,
-                        display: 'list-item',
-                        my: 0,
-                      }}
-                      primary={instruction}
-                      slotProps={{
-                        primary: {
-                          sx: {
-                            // backgroundColor: 'lightgrey',
-                            fontSize: '8pt',
-                          }
-                        }
-                      }}
-                    />
-                  </ListItem>
-                ))}
-              </List>
-            </Box>
-          </Grid>
-        </Grid>
+				<Grid container spacing={0}>
+					<Grid size={8}>
+						<Box>
+							<Typography
+								sx={{
+									fontWeight: "medium",
+									minHeight: 48,
+									lineHeight: 1.2,
+								}}
+							>
+								{title}
+							</Typography>
+							<Typography
+								sx={{
+									fontSize: 80,
+									fontWeight: "bold",
+									lineHeight: 0.8,
+									height: 64,
+								}}
+							>
+								{abbreviation}
+							</Typography>
+						</Box>
+					</Grid>
+					<Grid size={4} sx={{ position: "relative" }}>
+						<Box
+							component="img"
+							src={`${glassIconBasePath}${glassIcons[icon]}`}
+							sx={{ width: "100%", position: "absolute", bottom: 0 }}
+						></Box>
+					</Grid>
+					<Grid size={6}>
+						<Box sx={{ p: 1 }}>
+							<List dense sx={{ listStyleType: "disc", pl: 0 }}>
+								{ingredients.map((ingredient) => (
+									<ListItem key={ingredient} disablePadding>
+										<ListItemText
+											sx={{ fontSize: 8, display: "list-item", my: 0 }}
+											primary={ingredient}
+											slotProps={{ primary: { sx: { fontSize: "8pt" } } }}
+										/>
+									</ListItem>
+								))}
+							</List>
+						</Box>
+					</Grid>
+					<Grid size={6}>
+						<Box sx={{ p: 1 }}>
+							<List dense sx={{ listStyleType: "disc", pl: 0 }}>
+								{instructions.map((instruction) => (
+									<ListItem key={instruction} disablePadding>
+										<ListItemText
+											sx={{ fontSize: 8, display: "list-item", my: 0 }}
+											primary={instruction}
+											slotProps={{ primary: { sx: { fontSize: "8pt" } } }}
+										/>
+									</ListItem>
+								))}
+							</List>
+						</Box>
+					</Grid>
+				</Grid>
 			</CardContent>
 		</Card>
 	);
