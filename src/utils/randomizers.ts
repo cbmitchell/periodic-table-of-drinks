88b7bc4b1aeb44ad22a5fc48c6f4ffe8f5cf4385
Chris Mitchell <bobtheawesomechicken@gmail.com>
2026-02-24 14:06:04 -0600

    Add function for randomizing instructions

--- src/utils/randomizers.ts
+++ src/utils/randomizers.ts
@@ -3,8 +3,18 @@ import {
   type DrinkProps,
   type GlassIconName,
 } from '../components/DrinkCell'
+import {
+  randomTitleParts,
+  randomIngredientParts,
+  randomAmountParts,
+} from './randomizerParts'
 
-type IngredientType = 'spirit' | 'mixer' | 'flavor' | 'mouthfeel' | 'garnish'
+export type IngredientType =
+  | 'spirit'
+  | 'mixer'
+  | 'flavor'
+  | 'mouthfeel'
+  | 'garnish'
 
 export function weightedRandom(thresholds: number[]): number {
   const total = thresholds.reduce((sum, t) => sum + t, 0)
@@ -16,445 +26,13 @@ export function weightedRandom(thresholds: number[]): number {
   return thresholds.length - 1 // fallback for floating point edge cases
 }
 
-type NamePart = 'adverb' | 'adjective' | 'noun' | 'secondary_noun'
-const dummyTitleParts: Record<NamePart, string[]> = {
-  adverb: [
-    'Extremely',
-    'Dangerously',
-    'Accidentally',
-    'Unapologetically',
-    'Slightly',
-    'Mostly',
-    "Pete's",
-    "Grandma's",
-    'Undeniably',
-    'Eternally',
-    'Completely',
-    'Regretably',
-    'Technically',
-    'Incredibly',
-    'Unusually',
-    'Happily',
-    'Sadly',
-    'Supposedly',
-    'Allegedly',
-    'Entirely',
-    'Almost',
-    'Kinda',
-    'Really',
-    'Painfully',
-    'Powerfully',
-    'Upsettingly',
-    'Nice &',
-    'Pretty',
-    'Violently',
-    'Understandably',
-    'Graciously',
-    'Just a Real',
-    'Just a',
-    'Pleasingly',
-    'Very',
-    'Not',
-    'Flamboyantly',
-    'Terminally',
-    'Your',
-    'My',
-    "Everyone's",
-    "The People's",
-    "America's",
-    "The Queen's",
-    'Secretly',
-    'Badly',
-    'Unfortunately',
-    'Memory of a',
-    'Allegedly',
-    'The',
-  ],
-  adjective: [
-    'Little',
-    'Tiny',
-    'Muddy',
-    'Lovely',
-    'Normal',
-    'Average',
-    'Metaphorical',
-    'Last',
-    'Final',
-    'Louisiana',
-    'Tropical',
-    'Sideways',
-    'Springtime',
-    'Hot',
-    'Wet',
-    'Moist',
-    'Sleepy',
-    'Funky',
-    'Slippery',
-    'Fast',
-    'Dizzy',
-    'Ordinary',
-    'Complicated',
-    'Sophisticated',
-    'Philosophical',
-    'Tipsy',
-    'Upright',
-    'Regretful',
-    'Whimsical',
-    'Fairy',
-    'Drunk',
-    'Kinky',
-    'Funny',
-    'Ripped',
-    'Jacked',
-    'Exploding',
-    'Imploding',
-    'Crazy',
-    'Insane',
-    'Lively',
-    'Bad',
-    'American',
-    'Brazilian',
-    'Spanish',
-    'Canadian',
-    'English',
-    'Wumbly-Tumbly',
-    'Jiggly',
-    'Seething',
-    'Painful',
-    'Backwards',
-    'Black',
-    'Eldrich',
-    'Galactic',
-    'Celestial',
-    'Atmospheric',
-    'Powerful',
-    'Meaty',
-    'Aromatic',
-    'Fruity',
-    'Gay',
-    'Exciting',
-    'Old-Fashioned',
-    'Firey',
-    'Easy',
-    'Breezy',
-    'Dirty',
-    'Spicy',
-    'Heavenly',
-    'Naughty',
-    'Imaginary',
-    'Cool',
-    'Unhappy',
-    'Signature',
-    'Cold',
-    'Chilly',
-    'Special',
-    'Kiddie',
-    'Flamboyant',
-    'Deadly',
-    'Online',
-    'White',
-    'Blind',
-    'Royal',
-    'Secret',
-    'Poor',
-    'Slimy',
-    'Golden',
-    'Late-Night',
-    'New',
-    'Silly',
-    'Tragic',
-    'Xtreme',
-    'Heretical',
-    'Blasphemous',
-    'Rocky',
-    'Mysterious',
-    'Threatening',
-    'Dapper',
-    'Handsome',
-    'Forgotten',
-    'Foreign',
-    'Fat',
-    'Juicy',
-    'Sticky',
-    'Sweet',
-    'No',
-  ],
-  noun: [
-    'Explosion',
-    'Slap',
-    'Whisper',
-    'Terror',
-    'Mistake',
-    'Prospector',
-    'Corpse',
-    'Dipper',
-    'Dumper',
-    'Bucket',
-    'Train',
-    'Mountain',
-    'Racecar',
-    'President',
-    'Complication',
-    'Sophist',
-    'Philosopher',
-    'Gutpunch',
-    'Blackout',
-    'Fantasy',
-    'Shot',
-    'Cocktail',
-    'Catastrophe',
-    'Regret',
-    'King',
-    'Drink',
-    'Soup',
-    'Fizz',
-    'Sour',
-    'Slide',
-    'Hole',
-    'Cheek',
-    'Fart',
-    'Toilet',
-    'Mouthful',
-    'Punishment',
-    'Spank',
-    'Kaboose',
-    'Tooth',
-    'Treat',
-    'Echo',
-    'Motorboat',
-    'Breeze',
-    'Bluejay',
-    'Fool',
-    'Beartrap',
-    'Finger',
-    'Mouthwash',
-    'Communion',
-    'Booty',
-    'Promise',
-    'Dream',
-    'Hallucination',
-    'Signature',
-    'Handshake',
-    'Tea',
-    'Kiss',
-    'Lick',
-    'Jackpot',
-    'Flu',
-    'Kitten',
-    'Eruption',
-    'Cloud',
-    'Beating',
-    'Refreshment',
-    'Soap',
-    'Bath',
-    'Question',
-    'Fiesta',
-    'Secret',
-    'Dong',
-    'Whistle',
-    'Flip',
-    'Split',
-    'Protest',
-    'Nightmare',
-    'Event',
-    'Reminder',
-    'Jet',
-    'Dolphin',
-    'Tornado',
-    'Spine',
-    'Discipline',
-    'Apostle',
-    'Dick',
-    'Gold',
-    'Rizz',
-    'Parade',
-    'Answer',
-    'Solution',
-    'Posession',
-    'Interest',
-    'Song',
-    'Melody',
-    'Wife',
-    'Lover',
-    'Grandma',
-    'Senior Citizen',
-    'Heretic',
-    'Pipe',
-    'Rock',
-    'Heart',
-    'Splash',
-    'Mouth',
-    'Aphrodesiac',
-    'Surprise',
-    'Birthday',
-    'Opinion',
-    'Mystery',
-    'Breath',
-    'Gravestone',
-    'Wave',
-    'Alien',
-    'Princess',
-    'Bum',
-    'Juice',
-    'Nectar',
-  ],
-  secondary_noun: [
-    'Exploder',
-    'Cure',
-    'Reviver',
-    'Party',
-    'Killer',
-    'Corpse',
-    'Imploder',
-    'Giver',
-    'Dealer',
-    'Destroyer',
-    'Stealer',
-    'Slapper',
-    'Stopper',
-    'Ender',
-    'Licker',
-    'Eater',
-    'Spanker',
-    'Seducer',
-    'Maker',
-    'Juicer',
-    'Salesman',
-    'Spit',
-    'Lover',
-    'Stomper',
-    'for You',
-    'Slurper',
-    'Kisser',
-    'Flicker',
-    'Whisperer',
-    'Bucket',
-    'Hater',
-    'Extravaganza',
-    'Remix',
-    'Holder',
-    'House',
-    'Drink',
-    'On the Beach',
-    'Tester',
-    'Water',
-  ],
-}
-
-const dummyIngredients: Record<IngredientType, string[]> = {
-  spirit: [
-    'vodka',
-    'gin',
-    'rum',
-    'spiced rum',
-    'tequila',
-    'bourbon',
-    'whiskey',
-    'brandy',
-    'cognac',
-    'triple sec',
-    'cachaça',
-    'kahlua',
-    'irish cream liqueur',
-    'amaretto',
-    'moonshine',
-    'rubbing alcohol',
-  ],
-  mixer: [
-    'club soda',
-    'tonic water',
-    'ginger beer',
-    'cola',
-    'lemon-lime soda',
-    'cranberry juice',
-    'orange juice',
-    'lemon juice',
-    'lime juice',
-    'pineapple juice',
-    'grapefruit juice',
-    'tomato juice',
-    'pickle brine',
-    'boiling water',
-    'chicken broth',
-  ],
-  flavor: [
-    'bitters',
-    'orange bitters',
-    'chocolate bitters',
-    'cinnamon syrup',
-    'simple syrup',
-    'ginger syrup',
-    'st germain',
-    'miracle berry',
-    'soy sauce',
-  ],
-  mouthfeel: [
-    'egg white',
-    'whole milk',
-    'falernum',
-    'activated charcoal',
-    'methylcellulose',
-    'lecithin foam',
-    'small block of polonium',
-  ],
-  garnish: [
-    'lemon wheel',
-    'lemon twist',
-    'lemon wedge',
-    'lime wheel',
-    'lime twist',
-    'lime wedge',
-    'orange slice',
-    'orange wheel',
-    'orange twist',
-    'cherry',
-    'green olive',
-    'fresh mint sprig',
-    'cucumber slice',
-    'celery stalk',
-    'superfine sugar',
-    'cocktail umbrella',
-    'cinnamon stick',
-    'jalapeño slice',
-    'rainbow sprinkles',
-    'whipped cream',
-    'pop rocks',
-    'iron shavings',
-    'salt',
-    'pepper',
-    'ramen noodle flavor powder pack',
-    '#2 pencil',
-  ],
-}
-
 const randomAmount = () => {
-  const amounts = [
-    '¼',
-    '⅓',
-    '½',
-    '⅔',
-    '¾',
-    '1',
-    '1¼',
-    '1½',
-    '1¾',
-    '2',
-    '2½',
-    '3',
-    '4',
-    '6',
-    '12',
-  ]
+  const randomAmount =
+    randomAmountParts.quantity[
+      Math.floor(Math.random() * randomAmountParts.quantity.length)
+    ]
   const units = ['oz', 'tsp', 'gal']
-  const randomAmount = amounts[Math.floor(Math.random() * amounts.length)]
-  // const randomUnit = units[Math.floor(Math.random() * units.length)]
-  let randomUnit = 'oz'
-  const r = Math.random()
-  if (r < 0.1) {
-    randomUnit = 'gal'
-  } else if (r < 0.35) {
-    randomUnit = 'tsp'
-  }
+  const randomUnit = units[weightedRandom([75, 20, 5])]
   return `${randomAmount} ${randomUnit}`
 }
 
@@ -467,28 +45,33 @@ export const randomIngredients = (): string[] => {
 
   const possibleNumSpirits = [1, 2, 3, 4]
   const numSpirits = possibleNumSpirits[weightedRandom([75, 20, 4, 1])]
-  const spirits = pickRandom<string>(dummyIngredients.spirit, numSpirits).map(
-    (s) => `${randomAmount()} ${s}`,
-  )
+  const spirits = pickRandom<string>(
+    randomIngredientParts.spirit,
+    numSpirits,
+  ).map((s) => `${randomAmount()} ${s}`)
 
   const numMixers = weightedRandom([35, 40, 15, 10])
-  const mixers = pickRandom<string>(dummyIngredients.mixer, numMixers).map(
+  const mixers = pickRandom<string>(randomIngredientParts.mixer, numMixers).map(
     (m) => `${randomAmount()} ${m}`,
   )
 
   const numFlavors = weightedRandom([30, 50, 15, 5])
-  const flavors = pickRandom<string>(dummyIngredients.flavor, numFlavors).map(
-    (f) => `${randomAmount()} ${f}`,
-  )
+  const flavors = pickRandom<string>(
+    randomIngredientParts.flavor,
+    numFlavors,
+  ).map((f) => `${randomAmount()} ${f}`)
 
   const numMouthfeels = weightedRandom([60, 30, 8, 2])
   const mouthfeels = pickRandom<string>(
-    dummyIngredients.mouthfeel,
+    randomIngredientParts.mouthfeel,
     numMouthfeels,
   )
 
   const numGarnishes = weightedRandom([50, 40, 7, 3])
-  const garnishes = pickRandom<string>(dummyIngredients.garnish, numGarnishes)
+  const garnishes = pickRandom<string>(
+    randomIngredientParts.garnish,
+    numGarnishes,
+  )
 
   ingredientsList.push(...spirits)
   ingredientsList.push(...mixers)
@@ -499,22 +82,183 @@ export const randomIngredients = (): string[] => {
   return ingredientsList
 }
 
-// const dummyInstructions = {
-//   combine: [
-//     'Combine ingredients in ',
-//   ],
-//   mix: [
-//     'Combine ingredients in blender',
-//     'Dry shake ingredients in shaker',
-//     'Shake ingredients in shaker',
-//   ],
-//   garnish: [
-//     ''
-//     'Sneeze on it',
-//   ]
-// }
+const randomInstructionParts = {
+  prepare: [
+    'Prepare yourself',
+    'Call a loved one',
+    'Stretch',
+    'Mentally prepare',
+    'Put on latex gloves',
+    'Blindfold yourself',
+    'Spin until dizzy',
+    'Lay down tarp',
+    'Open a window',
+    'Pull yourself together',
+    'Go to your happy place',
+    'Ruminate',
+    'Ponder',
+    'Prepare the sacrifice',
+    'Pray',
+    'Ensure you are alone',
+    'Turn off all the lights',
+    'Light a scented candle',
+    'Apply eye protection',
+    'Write your will',
+    'Meditate',
+    'Slap it',
+    'Imagine a vast ocean',
+    'Go take a nap',
+    'Have someone assist you',
+    'Release your inhibitions',
+    'Put on some classical music',
+    'Wear safety goggles',
+    'Wait 20 minutes',
+    'Microwave 2 minutes',
+    'Consider next move',
+    'Check taste',
+    'Take a deep breath',
+    'Whisper a secret aloud',
+    'Do a gratitude exercise',
+    'Lock your doors',
+    'Close the blinds',
+    'Put on your drinking hat',
+    'Recite the incantation',
+    'Draw the runes',
+    'Perform blood sacrifice',
+    'Scroll social media',
+    'Play smooth jazz',
+    'Take a break',
+  ],
+  combine: [
+    'Build in glass',
+    'Build in glass',
+    'Combine in blender',
+    'Combine in shaker',
+    'Build in large cauldron',
+  ],
+  mix: [
+    'Blend',
+    'Dry shake',
+    'Shake',
+    'Stir',
+    'Stir with barspoon',
+    'Boil',
+    'Stir with large wooden spoon',
+    'Microwave',
+    'Chill',
+    'Beat the devil out of it',
+  ],
+  mix_until: [
+    'until dizzy',
+    'until wet',
+    'until you smell smoke',
+    'until clear',
+    'until cloudy',
+    'until just right',
+    'until gasseous',
+    'until smooth',
+    'until thick',
+    'until frothy',
+    'until satisfied',
+    'until delicious',
+    "until you can't anymore",
+    'untkl viscous',
+    'until done',
+    'a few times',
+    'for 30 seconds',
+    'for 2 minutes',
+    'vigorously',
+    'violently',
+    'well',
+    'for a while',
+  ],
+  garnish: [
+    'Issue positive affirmations',
+    'Sneeze on it',
+    'Top with garnish',
+    'Coat rim of glass',
+    'Flip upside-down briefly',
+    'Light on fire',
+    'Decorate',
+    'Sign your name on it',
+  ],
+  serve: [
+    'Enjoy',
+    'Enjoy',
+    'Enjoy',
+    'Serve to enemy',
+    'Throw in the trash',
+    'Appreciate',
+    'Do not drink',
+    'Do not serve',
+    'Dump in the toilet',
+    'Serve to close friends',
+    'Serve & enjoy',
+    'Serve under cloche',
+    'Take a photo & post it',
+    'Let sit overnight',
+    'Drink with caution',
+    'Drink at your own risk',
+    'Sip slowly',
+    'Share with friends',
+  ],
+}
 
-const generateRandomInstructions = () => {}
+const generateRandomInstructions = (): string[] => {
+  const instructions: string[] = []
+  const MAX_PREPARATIONS = 4
+  const preparations: string[] = pickRandom<string>(
+    randomInstructionParts.prepare,
+    MAX_PREPARATIONS,
+  )
+  if (Math.random() < 0.3 && preparations.length > 0) {
+    instructions.push(preparations.shift()!)
+  }
+  if (Math.random() < 0.1 && preparations.length > 0) {
+    instructions.push(preparations.shift()!)
+  }
+  instructions.push(
+    randomInstructionParts.combine[
+      Math.floor(Math.random() * randomInstructionParts.combine.length)
+    ],
+  )
+  if (Math.random() < 0.15 && preparations.length > 0) {
+    instructions.push(preparations.shift()!)
+  }
+  if (Math.random() < 0.8) {
+    let mix =
+      randomInstructionParts.mix[
+        Math.floor(Math.random() * randomInstructionParts.mix.length)
+      ]
+    if (Math.random() < 0.5) {
+      const mix_until =
+        randomInstructionParts.mix_until[
+          Math.floor(Math.random() * randomInstructionParts.mix_until.length)
+        ]
+      mix = mix.concat(` ${mix_until}`)
+    }
+    instructions.push(mix)
+  }
+  if (Math.random() < 0.1 && preparations.length > 0) {
+    instructions.push(preparations.shift()!)
+  }
+  if (Math.random() < 0.3) {
+    instructions.push(
+      randomInstructionParts.garnish[
+        Math.floor(Math.random() * randomInstructionParts.garnish.length)
+      ],
+    )
+  }
+  if (Math.random() < 0.75) {
+    instructions.push(
+      randomInstructionParts.serve[
+        Math.floor(Math.random() * randomInstructionParts.serve.length)
+      ],
+    )
+  }
+
+  return instructions
+}
 
 const generateRandomTitle = (): string => {
   let title = ''
@@ -522,37 +266,36 @@ const generateRandomTitle = (): string => {
 
   if (Math.random() < 0.2) {
     const adverb =
-      dummyTitleParts.adverb[
-        Math.floor(Math.random() * dummyTitleParts.adverb.length)
+      randomTitleParts.adverb[
+        Math.floor(Math.random() * randomTitleParts.adverb.length)
       ]
     title = title.concat(`${adverb} `)
   }
 
   if (Math.random() < 0.8 || title.length > 0) {
     const adjective =
-      dummyTitleParts.adjective[
-        Math.floor(Math.random() * dummyTitleParts.adjective.length)
+      randomTitleParts.adjective[
+        Math.floor(Math.random() * randomTitleParts.adjective.length)
       ]
     title = title.concat(`${adjective} `)
     adjectivePresent = true
   }
 
   const noun =
-    dummyTitleParts.noun[
-      Math.floor(Math.random() * dummyTitleParts.noun.length)
+    randomTitleParts.noun[
+      Math.floor(Math.random() * randomTitleParts.noun.length)
     ]
   title = title.concat(noun)
 
-  const secondaryNounThreshold = adjectivePresent ? 0.25 : 0.75
+  const secondaryNounThreshold = adjectivePresent ? 0.2 : 0.75
   if (Math.random() < secondaryNounThreshold) {
     const secondary_noun =
-      dummyTitleParts.secondary_noun[
-        Math.floor(Math.random() * dummyTitleParts.secondary_noun.length)
+      randomTitleParts.secondary_noun[
+        Math.floor(Math.random() * randomTitleParts.secondary_noun.length)
       ]
     title = title.concat(` ${secondary_noun}`)
   }
 
-  console.log(title)
   return title
 }
 
@@ -588,5 +331,6 @@ export function generateRandomDrink(): DrinkProps {
     abbreviation: generateAbbreviation(title),
     icon: randomIcon(),
     ingredients: randomIngredients(),
+    instructions: generateRandomInstructions(),
   }
 }
